44133619493fb9371ce94c6804cb22fc96eec1da
Jordan <jordan.p62@outlook.fr>
2026-01-05 22:56:16 +0100

    working on booster

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,5 +1,6 @@
 import { OrbitControls, useTexture } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
+import { useControls } from "leva";
 import { Perf } from "r3f-perf";
 import { useEffect } from "react";
 import * as THREE from "three";
@@ -9,6 +10,23 @@ export default function Experience() {
   const model = useLoader(GLTFLoader, "./booster.glb");
   const textureRecto = useTexture("./boosterS1Recto.webp");
   const textureVerso = useTexture("./boosterS1Verso.webp");
+
+  const {
+    textureRectoRepeat,
+    textureRectoOffset,
+    textureVersoRepeat,
+    textureVersoOffset,
+  } = useControls("texture", {
+    textureRectoRepeat: { value: [1.81, 1.25] },
+    textureRectoOffset: { value: [0.06, -0.19] },
+    textureVersoRepeat: { value: [1.9, 1.5] },
+    textureVersoOffset: { value: [-0.89, -0.13] },
+  });
+
+  const { metalness, roughness } = useControls("material", {
+    metalness: { value: 0.3, min: 0, max: 1, step: 0.01 },
+    roughness: { value: 0.3, min: 0, max: 1, step: 0.01 },
+  });
 
   // 🔧 fixes textures
   textureRecto.flipY = false;
@@ -21,29 +39,33 @@ export default function Experience() {
   textureVerso.wrapS = textureVerso.wrapT = THREE.ClampToEdgeWrapping;
 
   // Ajuster la taille et le centrage des textures
-  textureRecto.repeat.set(1.5, 1.5);
-  textureRecto.offset.set(0.25, -0.5);
+  textureRecto.repeat.set(...textureRectoRepeat);
+  textureRecto.offset.set(...textureRectoOffset);
 
-  textureVerso.repeat.set(0.5, 0.5);
-  textureVerso.offset.set(0.25, 0.25);
+  textureVerso.repeat.set(...textureVersoRepeat);
+  textureVerso.offset.set(...textureVersoOffset);
 
   useEffect(() => {
     model.scene.traverse((child) => {
       if (child.isMesh) {
+        console.log("Material name:", child.material.name);
+
         if (child.material.name === "Booster_front") {
           child.material.map = textureRecto;
+          child.material.metalness = metalness;
+          child.material.roughness = roughness;
         }
         if (child.material.name === "Booster_back") {
           child.material.map = textureVerso;
+          child.material.metalness = metalness;
+          child.material.roughness = roughness;
         }
 
-        child.material.metalness = 0;
-        child.material.roughness = 1;
         child.material.side = THREE.FrontSide;
         child.material.needsUpdate = true;
       }
     });
-  }, [model, textureRecto, textureVerso]);
+  }, [model, textureRecto, textureVerso, metalness, roughness]);
 
   return (
     <>
@@ -51,9 +73,11 @@ export default function Experience() {
       <OrbitControls makeDefault />
 
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
-      <ambientLight intensity={1.5} />
+      <ambientLight intensity={5.5} />
 
-      <primitive object={model.scene} />
+      <primitive object={model.scene}>
+        <meshPhongMaterial color={"white"} />
+      </primitive>
     </>
   );
 }
